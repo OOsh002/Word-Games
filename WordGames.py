b339204759d23738e4c2b9aff7eadab17564fc7d
Aayush Goyal <Aayush@MacBook-Pro-27.lan>
2020-09-09 14:01:43 -0700

    Current Progress so far

---
+++ WordGames.py
@@ -0,0 +1,12 @@
+
+def main():
+	with open('names.txt', 'r') as f:
+		for line in f: 
+			print(line)
+
+
+# Write a program (game: Ghost) where the user and the computer play the game together
+# OR write one for two players
+
+if __name__ == '__main__':
+	main()
